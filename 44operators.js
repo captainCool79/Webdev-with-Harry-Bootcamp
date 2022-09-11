6ee78afaf49571077d1a37921ae9ff3cb84b9463
Aditya Raj <79632537+adityaraj79@users.noreply.github.com>
2022-09-11 23:56:27 +0530

    Create 44operators.js

---
+++ 44operators.js
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script>
+        var a=12;
+        var b="Adi";
+        c = 13.32;
+        console.log(c);
+
+        //Operators in JavaScript
+
+        //Operand -
+        //In 3+4 +is the operator and 3,4 are the operands
+        //1. unary operator - It has single operand (x= -x)
+        //Examples of unary operator
+        c=-c;
+        console.log(c);
+
+        //2. binary operator - It has two operands (x = x+6)
+        //Eamples of binary operator
+        c=43+23;
+        console.log(c);
+
+        var num1=3;
+        var num2=6;
+
+        //Arithmatic operators in action in Javascript
+        console.log("The value of num1+num2 is "+ (num1+num2));
+        console.log("The value of num1-num2 is "+ (num1-num2));
+        console.log("The value of num1*num2 is "+ (num1*num2));
+        console.log("The value of num1/num2 is "+ (num1/num2));
+        console.log("The value of num1**num2 is "+ (num1**num2));
+        console.log("The value of ++num1 is "+ (++num1));
+
+
+
+    </script>
+</head>
+<body>
+    <div class="container">
+        <h1>This is a heading.</h1>
+        <div class="content">
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum pariatur quasi magni unde, voluptate excepturi doloremque deserunt nostrum? Consectetur amet sapiente ex quaerat enim, hic velit omnis molestiae vitae, nesciunt est ad neque, dolores suscipit cupiditate. Tempore dicta, natus possimus ut doloribus ipsum in dolorem numquam eos, quaerat doloremque, nostrum ducimus deleniti nihil fugit?
+            </p>
+        </div>
+    </div>
+</body>
+</html>
